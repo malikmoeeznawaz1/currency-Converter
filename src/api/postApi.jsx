abfc64fa6a99500fbbe8efe9fddb34309e7dfb49
Malik Moeez Nawaz <malikmoeez.nawaz@studentambassadors.com>
2025-02-21 01:54:00 +0500

    configuring api

--- src/api/postApi.jsx
+++ src/api/postApi.jsx
@@ -3,3 +3,9 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://v6.exchangerate-api.com/v6/12adef065bedde8125f3121a",
 });
+
+export const currencyConverter = async (fromCurrency, toCurrency, amount) => {
+  const res = await api.get(`/pair/${fromCurrency}/${toCurrency}/${amount}`);
+  console.log(res);
+  return res.data.conversion_result;
+};
