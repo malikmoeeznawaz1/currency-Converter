3c5296251b7a48dc19f3ad188f4f07e05621b8b1
Malik Moeez Nawaz <malikmoeez.nawaz@studentambassadors.com>
2025-02-27 01:06:35 +0500

    design is almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const[amount, setAmount] = useState(0);
+  const[fromCurrency, setCurrency] = useState("USD");
+  const[toCurrency, setToCurrency] = useState("PKR");
+
   return <section>
-    
+    <div className="currency-converter">
+      <div className='currency-div'>
+        <h1>Currency Converter</h1>
+        <hr />
+        <div>
+          <label >
+            Amount:
+            <input type="text" value={amount} onChange={(e)=>setAmount(e.target.value)}/>
+          </label>
+        </div>
+        <section className="currency-selector">
+          <label>
+            From:
+            <select>
+              {
+                ["USD", "EUR", "PKR", "GBP", "AUD"].map((currency)=>{
+                  return <option key={currency} value={currency}>{currency}</option>
+                })
+              }
+            </select>
+          </label>
+
+          <label>
+            To:
+            <select>
+              {
+                ["EUR", "USD", "AUD", "GBP", "PKR"].map((currency)=>{
+                  return <option key={currency} value={currency}>{currency}</option>
+                })
+              }
+            </select>
+          </label>
+        </section>
+
+        <button disabled={isLoading || amount <= 0}>{isLoading? "converting..." : "Convert"}</button>
+      </div>
+    </div>
   </section>
 }
 
